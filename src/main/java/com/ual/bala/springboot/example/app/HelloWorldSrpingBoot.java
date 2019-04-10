98bd8e742dff9d6c0c136a896cded5050ac98873
RAO <n082615@global.ual.com>
2019-04-10 15:01:33 -0500

    first commit

---
+++ src/main/java/com/ual/bala/springboot/example/app/HelloWorldSrpingBoot.java
@@ -0,0 +1,15 @@
+package com.ual.bala.springboot.example.app;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSrpingBoot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SpringApplication.run(HelloWorldSrpingBoot.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/ual/bala/springboot/example/app/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.ual.bala.springboot.example.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping(value="/")
+	public String hello() {		
+		return "Hello to Spring Boot example";
+		
+	}
+
+}
